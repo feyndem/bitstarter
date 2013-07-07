9e788bbe4fbece51e1a45e0dda047f79b4dfadb0
feyndem <feyn.dem@gmail.com>
2013-07-07 16:50:18 +0200

    Update grader.js

--- grader.js
+++ grader.js
@@ -24,7 +24,6 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,15 +64,11 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-u, --url <check-url>', 'Path to URL')
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-     rest.get(program.url).on('complete', function(result) {
-       fs.writeFile("index3.html", result);
-       if(assertFileExists("index3.html")){
-       var checkJson = checkHtmlFile("index3.html", program.checks);
-       var outJson = JSON.stringify(checkJson, null, 4);
-       console.log(outJson);}
-     });
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
